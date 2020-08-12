3c6872bbd58586769442dd9e6242c798584da5be
Seoyoung Kim <48192106+vvvpiano@users.noreply.github.com>
2020-08-12 23:06:26 +0900

    after review2

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -3,6 +3,16 @@ import java.util.List;
 
 public class Point {
     private static final int NUMBER_OF_DIMENSION = 2;
+
+    public String getXY (String input) {
+        input = removeParenthesis(input);
+        List<Integer> pointNumbers = getNumbers(input);
+        String validation = validateNumbers(pointNumbers);
+        if (validation != "SUCCESS")
+            return validation;
+        return "x=" + pointNumbers.get(0) + ", y=" + pointNumbers.get(1);
+    }
+
     private String removeParenthesis(String input) {
         if (input.charAt(0) == '(') {
             input = input.substring(1);
@@ -13,33 +23,27 @@ public class Point {
         return input;
     }
 
-    private ArrayList<Integer> getNumberArray(String input) {
-        String[] stringNumberArray = input.split(",");
-        ArrayList<Integer> numberArray = new ArrayList<>();
-        for (int i = 0; i < stringNumberArray.length; i++) {
-            numberArray.add(Integer.parseInt(stringNumberArray[i].trim()));
+    private List<Integer> getNumbers(String input) {
+        String[] numberTexts = input.split(",");
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < numberTexts.length; i++) {
+            numbers.add(Integer.parseInt(numberTexts[i].trim()));
         }
-        return numberArray;
+        return numbers;
     }
 
-    private String validateNumberArray(ArrayList<Integer> arr) {
-        if (arr.size() < NUMBER_OF_DIMENSION)
+    private String validateNumbers(List<Integer> numbers) {
+        if (numbers.size() < NUMBER_OF_DIMENSION) {
             return "Insufficient factors";
-        if (arr.size() > NUMBER_OF_DIMENSION)
+        }
+        if (numbers.size() > NUMBER_OF_DIMENSION) {
             return "Too many factors";
+        }
         for (int i = 0; i < NUMBER_OF_DIMENSION; i++) {
-            if (arr.get(i) < 1 || 24 < arr.get(i))
-                return "Not a valid number: "+ arr.get(i);
+            if (numbers.get(i) < 1 || 24 < numbers.get(i)) {
+                return "Not a valid number: "+ numbers.get(i);
+            }
         }
         return "SUCCESS";
-    }
-
-    public String getXY (String input) {
-        input = removeParenthesis(input);
-        ArrayList<Integer> pointArr = getNumberArray(input);
-        String validation = validateNumberArray(pointArr);
-        if (validation != "SUCCESS")
-            return validation;
-        return "x=" + pointArr.get(0) + ", y=" + pointArr.get(1);
     }
 }
