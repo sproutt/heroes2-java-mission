cfc2b705745e984622c7e7c9ceed7e8d780c5d1e
Seoyoung Kim <48192106+vvvpiano@users.noreply.github.com>
2020-08-17 14:44:11 +0900

    InputViewException class

---
+++ src/main/java/InputViewException.java
@@ -0,0 +1,9 @@
+public class InputViewException extends Exception {
+    InputViewException() {
+        super("Name Exception Occurred");
+    }
+
+    InputViewException(String message) {
+        super(message);
+    }
+}
