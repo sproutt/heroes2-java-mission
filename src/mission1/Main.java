8dc26944e1f02ef9c329efa12e6193a538bca94f
GlassK <glassk0724@gmail.com>
2020-08-20 00:01:20 +0900

    Main Class

---
+++ src/mission1/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+        RacingGame racingGame = new RacingGame();
+        GameWinner gameWinner = new GameWinner();
+        ResultView resultView = new ResultView();
+
+        Car[] car = inputView.inputCarName();
+        int time = inputView.inputTime();
+        System.out.println("\n실행 결과");
+        for (int i = 0; i < time; i++) {
+            racingGame.raceByTime(car);
+            resultView.showResultByTime(car);
+            System.out.println();
+        }
+        resultView.showFinalResult(gameWinner.findWinner(car));
+    }
+}
