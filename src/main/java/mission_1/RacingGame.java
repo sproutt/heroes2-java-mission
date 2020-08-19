0d7c96b4d1573dce2dc7f40b80dd05fca2dcd85d
kcy13930 <kcy13930@gmail.com>
2020-08-19 18:21:16 +0900

    Business Logic

---
+++ src/main/java/mission_1/RacingGame.java
@@ -0,0 +1,51 @@
+package MISSION_1;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class RacingGame {
+    private int time = 0;
+    private ArrayList<String> car = new ArrayList<>();
+    private ArrayList<Integer> carPosition = new ArrayList<>();
+    private Random rand = new Random();
+
+    public RacingGame(){}
+    public RacingGame(int seed){ rand.setSeed(seed); }
+
+    public void setCar(String input){
+        String[] carNames = input.split(",");
+        for(String carName : carNames){
+            car.add(carName.trim());
+            carPosition.add(0);
+        }
+    }
+
+    public int getTime(){ return time; }
+
+    public ArrayList getCar(){ return car; }
+
+    public ArrayList getCarPosition(){ return carPosition; }
+
+    public void setTime(int time){ this.time = time; }
+
+    public void move(){
+        for(int index = 0; index < car.size(); index++){
+            int condition = rand.nextInt(10);
+            if(condition >= 4 && condition <= 9){
+                carPosition.set(index,carPosition.get(index)+1);
+            }
+        }
+    }
+    
+    public String winner(){
+        ArrayList<String> winner = new ArrayList<>();
+        Integer maxValue = (Integer) Collections.max(carPosition);
+        for(int i=0; i< carPosition.size(); i++){
+            if(carPosition.get(i) == maxValue){
+                winner.add(car.get(i));
+            }
+        }
+        String winnerList = String.join(",",winner.toArray(new String[winner.size()]));
+        return winnerList;
+    }
+}
---
+++ src/main/java/mission_1/RacingGameTest.java
@@ -0,0 +1,72 @@
+package MISSION_1;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.Random;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class RacingGameTest {
+    RacingGame game;
+    String input;
+    int seed = 2020;
+    Random rand = new Random();
+
+    @Before
+    public void setUp(){
+        rand.setSeed(seed);
+        game = new RacingGame(seed);
+    }
+
+    @Test
+    public void setCarTest() {
+        input = "Window0, KeyCat, Cloud, Grid, Asong";
+        game.setCar(input);
+
+        ArrayList<String> car = game.getCar();
+        Iterator<String> itr = car.iterator();
+
+        assertEquals("Window0", itr.next());
+        assertEquals("KeyCat", itr.next());
+        assertEquals("Cloud", itr.next());
+        assertEquals("Grid", itr.next());
+        assertEquals("Asong", itr.next());
+    }
+
+    @Test
+    public void moveTest() {
+        game.move();
+        ArrayList<String> carPosition = game.getCarPosition();
+        Iterator<String> itr = carPosition.iterator();
+
+        while(itr.hasNext()){
+            int condition = rand.nextInt(10);
+            if(condition>=4 && condition <=9){
+                assertEquals(1, itr.next());
+            }
+            if(condition>=0 && condition <=3){
+                assertEquals(0, itr.next());
+            }
+        }
+    }
+
+    @Test
+    public void winnerTest(){
+        input = "Window0, KeyCat, Cloud, Grid, Asong";
+        game.setCar(input);
+        game.move();
+
+        ArrayList<Integer> carPosition = game.getCarPosition();
+        Integer maxValue = (Integer) Collections.max(carPosition);
+        assertEquals(game.getCar().get(carPosition.indexOf(maxValue)),
+                game.winner().split(",")[0]);
+    }
+    @After
+    public void cleanUp() { game = null; }
+}
